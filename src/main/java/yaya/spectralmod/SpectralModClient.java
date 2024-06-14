c3a1e197e4fa6fdb130442ce0fd01672885ae307
yayajda <106956238+yayajda@users.noreply.github.com>
2024-06-15 00:35:14 +0800

    开发环境配置

---
+++ src/main/java/yaya/spectralmod/SpectralModClient.java
@@ -0,0 +1,10 @@
+package yaya.spectralmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpectralModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
